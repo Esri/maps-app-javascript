a84e9206564a59b9b2737ae477d9c442681cfc36
rene rubalcava <rrubalcava@esri.com>
2017-11-01 11:37:00 -0700

    accidentally deleted config/main

---
+++ src/config/main.ts
@@ -0,0 +1,15 @@
+export const appId = "zZT62Q2mz1sbArWw";
+
+export const portalUrl = "https://odoenet.maps.arcgis.com";
+
+export const routeUrl =
+  "https://route.arcgis.com/arcgis/rest/services/World/Route/NAServer/Route_World";
+
+export const geocodeUrl =
+  "https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer";
+
+export const webMapItem = {
+  portalItem: {
+    id: "1aab2defd7444b6790f439a186cd4a23"
+  }
+};
